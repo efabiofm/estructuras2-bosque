a8998c4e1d680fc45f47dbd1c1311c7caff3b211
Rodrigo Paris <rodrigo.paris@ciriscr.com>
2017-06-25 18:45:12 -0600

    Implementacion de Arbol B
    - Insertar
    - Buscar
    - Imprimir

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Estructuras2-Bosque
+PROJECTNAME=estructuras2-bosque
 
 # Active Configuration
 DEFAULTCONF=Debug
